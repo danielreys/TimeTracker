0c9d84baea3942f6b40a26254c1bdc5fda7b1c21
danireyes5015@gmail.com <50138013>
2019-10-04 11:10:05 +0200

    update
    function delete

--- results.php
+++ results.php
@@ -19,14 +19,6 @@ $sth = $conn->query($query);
 while( $row = $sth->fetch(PDO::FETCH_ASSOC) ) {
   $registers[] = $row; // appends each row to the array
 }
-function sumarHoras($horas) {
-    $total = 0;
-    foreach($horas as $h) {
-        $parts = explode(":", $h);
-        $total += $parts[2] + $parts[1]*60 + $parts[0]*3600;        
-    }   
-    return gmdate("H:i:s", $total);
-}
     ?>
 <table>
      <thead>
